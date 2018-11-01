19516244c2b5003a69acc489e308786196783517
kilia <kilia@DESKTOP-E43KSBT.speedport.ip>
2018-11-01 15:05:39 +0100

    Ergänzung zum Loadingscreen

--- Minesweeper/src/Field.java
+++ Minesweeper/src/Field.java
@@ -10,13 +10,38 @@ public class Field extends JButton{
 	private int y;
 	private boolean isBomb;
 	private Image img;
+	private boolean inGame;
 	
+
+
+
 	public Field (int x,int y, boolean bomb) {
 		super();
 		this.x = x;
 		this.y=y;
 		this.isBomb=bomb;
+		inGame=false;
 		
+			try {
+				img = ImageIO.read(getClass().getResource("pictures/Bombe.png"));
+//				f.setIcon(new ImageIcon(img.getScaledInstance(
+//						(int)((Math.min(frame.getHeight(), frame.getWidth()) / realBoard.length)/1.25),
+//						(int)((Math.min(frame.getHeight(), frame.getWidth()) / realBoard.length)/1.25), Image.SCALE_FAST)));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		
+	}
+	
+	public boolean isInGame() {
+		return inGame;
+	}
+
+
+	public void setInGame(boolean inGame) {
+		this.inGame = inGame;
 	}
 	
 
@@ -52,17 +77,7 @@ public class Field extends JButton{
 	}
 
 	public void setBomb(boolean isBomb) {
-		if(isBomb) {
-			try {
-				img = ImageIO.read(getClass().getResource("pictures/Bombe.png"));
-//				f.setIcon(new ImageIcon(img.getScaledInstance(
-//						(int)((Math.min(frame.getHeight(), frame.getWidth()) / realBoard.length)/1.25),
-//						(int)((Math.min(frame.getHeight(), frame.getWidth()) / realBoard.length)/1.25), Image.SCALE_FAST)));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+
 		this.isBomb = isBomb;
 	}
 
